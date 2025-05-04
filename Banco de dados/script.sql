e3de024192866409d4501cac0bb6b0c41ccad651
gustavokeniti <gustavo.keniti@sptech.school>
2025-05-03 22:08:49 -0300

    alterando index do site

--- Banco de dados/script.sql
+++ Banco de dados/script.sql
@@ -1,24 +1,7 @@
 CREATE DATABASE basketmetrics;
 USE basketmetrics;
 
-CREATE TABLE usuario (
-idusuario INT NOT NULL AUTO_INCREMENT,
-nome VARCHAR(45) NOT NULL,
-telefone VARCHAR(45) NOT NULL,
-email VARCHAR(45) NOT NULL,
-senha VARCHAR(45) NOT NULL,
-fkendereco INT NOT NULL,
-fkskill INT NOT NULL,
-PRIMARY KEY (idusuario),
-CONSTRAINT fk_usuario_endereco
-FOREIGN KEY (fkendereco)
-REFERENCES mydb.endereco (idendereco),
-CONSTRAINT fk_usuario_skill1
-FOREIGN KEY (fkskill)
-REFERENCES skill (idskill)
-);
-
-CREATE TABLE endereco (
+CREATE TABLE IF NOT EXISTS endereco (
 idendereco INT NOT NULL AUTO_INCREMENT,
 rua VARCHAR(45) NOT NULL,
 numero INT NOT NULL,
@@ -27,7 +10,7 @@ cidade VARCHAR(45) NOT NULL,
 PRIMARY KEY (idendereco)
 );
 
-CREATE TABLE skill (
+CREATE TABLE IF NOT EXISTS skill (
 idskill INT NOT NULL AUTO_INCREMENT,
 idade INT NOT NULL,
 altura DECIMAL(5,2) NOT NULL,
@@ -38,4 +21,36 @@ drible INT NOT NULL,
 controle INT NOT NULL,
 defesa INT NOT NULL,
 PRIMARY KEY (idskill)
+);
+
+CREATE TABLE IF NOT EXISTS jogador (
+idjogador INT NOT NULL AUTO_INCREMENT,
+nome VARCHAR(45) NOT NULL,
+sobrenome VARCHAR(45) NOT NULL,
+telefone CHAR(14) NOT NULL,
+email VARCHAR(45) NOT NULL,
+senha VARCHAR(45) NOT NULL,
+fkendereco INT NOT NULL,
+fkskill INT NOT NULL,
+PRIMARY KEY (idjogador),
+CONSTRAINT fk_usuario_endereco1
+FOREIGN KEY (fkendereco)
+REFERENCES endereco (idendereco),
+CONSTRAINT fk_jogador_copy1_skill1
+FOREIGN KEY (fkskill)
+REFERENCES skill (idskill)
+);
+
+CREATE TABLE IF NOT EXISTS tecnico (
+idtecnico INT NOT NULL AUTO_INCREMENT,
+nome VARCHAR(45) NOT NULL,
+sobrenome VARCHAR(45) NOT NULL,
+telefone CHAR(14) NOT NULL,
+email VARCHAR(45) NOT NULL,
+senha VARCHAR(45) NOT NULL,
+fkendereco INT NOT NULL,
+PRIMARY KEY (idtecnico),
+CONSTRAINT fk_usuario_endereco2
+FOREIGN KEY (fkendereco)
+REFERENCES endereco (idendereco)
 );
