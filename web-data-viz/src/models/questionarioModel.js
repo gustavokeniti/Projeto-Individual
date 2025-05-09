0c5c3a29e1d7d94d2687f1ba1f22aa5fe195c84b
gustavokeniti <gustavo.keniti@sptech.school>
2025-05-09 09:37:58 -0300

    ajustes questionario

--- web-data-viz/src/models/questionarioModel.js
+++ web-data-viz/src/models/questionarioModel.js
@@ -2,7 +2,7 @@ var database = require("../database/config")
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
 function preencherQues(idade, altura, posicao, arremesso, passe, drible, controle, defesa, fkjogador) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", idade, altura, posicao, arremesso, passe, drible, controle, defesa, fkjogador);
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function preencherQues():", idade, altura, posicao, arremesso, passe, drible, controle, defesa, fkjogador);
     
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
