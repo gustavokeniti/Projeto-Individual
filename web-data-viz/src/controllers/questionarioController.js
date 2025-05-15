388a9d755dd7905af064f707bee2e12c731ecf11
gustavokeniti <gustavo.keniti@sptech.school>
2025-05-15 00:03:15 -0300

    adicionando calculo de media geral no questionario

--- web-data-viz/src/controllers/questionarioController.js
+++ web-data-viz/src/controllers/questionarioController.js
@@ -10,6 +10,7 @@ function preencherQues(req, res) {
     var drible = req.body.dribleServer;
     var controle = req.body.controleServer;
     var defesa = req.body.defesaServer;
+    var mediaGeral = req.body.mediaGeralServer;
     var fkjogador = req.body.idJogadorVincularServer;
 
     // Faça as validações dos valores
@@ -29,12 +30,14 @@ function preencherQues(req, res) {
         res.status(400).send("Seu controle está undefined!");
     } else if (defesa == undefined) {
         res.status(400).send("Sua defesa está undefined!");
-    } else if (fkjogador == undefined) {
+    } else if (mediaGeral == undefined) {
+        res.status(400).send("Sua média está undefined!");
+    }else if (fkjogador == undefined) {
         res.status(400).send("Sua fk está undefined!");
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        questionarioModel.preencherQues(idade, altura, posicao, arremesso, passe, drible, controle, defesa, fkjogador)
+        questionarioModel.preencherQues(idade, altura, posicao, arremesso, passe, drible, controle, defesa, mediaGeral, fkjogador)
             .then(
                 function (resultado) {
                     res.json(resultado);
--- web-data-viz/src/models/questionarioModel.js
+++ web-data-viz/src/models/questionarioModel.js
@@ -1,13 +1,13 @@
 var database = require("../database/config")
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
-function preencherQues(idade, altura, posicao, arremesso, passe, drible, controle, defesa, fkjogador) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function preencherQues():", idade, altura, posicao, arremesso, passe, drible, controle, defesa, fkjogador);
+function preencherQues(idade, altura, posicao, arremesso, passe, drible, controle, defesa, mediaGeral, fkjogador) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function preencherQues():", idade, altura, posicao, arremesso, passe, drible, controle, defesa, mediaGeral, fkjogador);
     
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-        INSERT INTO skill (idade, altura, posicao, arremesso, passe, drible, controle, defesa, fkjogador) VALUES ('${idade}', '${altura}', '${posicao}', '${arremesso}', '${passe}', '${drible}', '${controle}', '${defesa}', '${fkjogador}');
+        INSERT INTO skill (idade, altura, posicao, arremesso, passe, drible, controle, defesa, mediaGeral, fkjogador) VALUES ('${idade}', '${altura}', '${posicao}', '${arremesso}', '${passe}', '${drible}', '${controle}', '${defesa}', '${mediaGeral}', '${fkjogador}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
